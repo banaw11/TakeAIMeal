ba9f1eee08e7e1d50434f4f0c678177d2c1ebca8
banaw11 <banaw11@gmail.com>
2023-04-15 14:06:49 +0200

    Add text recognition service

---
+++ TakeAIMeal.Common.Services/Logic/TextRecognitionService.cs
@@ -0,0 +1,47 @@
+﻿using TakeAIMeal.Common.Services.Interfaces;
+using TakeAIMeal.Common.Services.Models.AzureCognitive;
+
+namespace TakeAIMeal.Common.Services.Logic
+{
+    public class TextRecognitionService : ITextRecognitionService
+    {
+        private readonly ICognitiveLanguageAPI _cognitiveLanguageAPI;
+
+        public TextRecognitionService(ICognitiveLanguageAPI cognitiveLanguageAPI)
+        {
+            _cognitiveLanguageAPI = cognitiveLanguageAPI;
+        }
+
+        public async Task<ICollection<DocumentEntity>> GetTagsFromText(string text, string language = "en")
+        {
+            if (!string.IsNullOrEmpty(text))
+            {
+                TextRecognitionBody body = new TextRecognitionBody
+                {
+                    Documents = new List<RecognitionDocument>
+                    {
+                        new RecognitionDocument
+                        {
+                            Language = language,
+                            Text = text
+                        }
+                    }
+                };
+
+                try
+                {
+                    var response = await _cognitiveLanguageAPI.GetTextRecognition(body);
+                    if(response != null && response.Documents != null && response.Documents.Count > 0)
+                    {
+                        return response.Documents.FirstOrDefault().Entities;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    //To-do error handling
+                }
+            }
+            return null;
+        }
+    }
+}
