3d2ff6d699d4234cf14925e31d1421192f7edfb4
nazwa <bla@bla.pl>
2023-04-15 23:51:23 +0200

    zmiany

--- TakeAIMeal.WebUI/src/App.vue
+++ TakeAIMeal.WebUI/src/App.vue
@@ -1,26 +1,32 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <HeaderComponent />
+  <NavComponent />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import HeaderComponent from './components/Header.vue'
+import NavComponent from './components/Nav.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    HeaderComponent,
+    NavComponent
   }
 }
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
---
+++ TakeAIMeal.WebUI/src/components/Nav.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>TEST</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'NavComponent'
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
+  
