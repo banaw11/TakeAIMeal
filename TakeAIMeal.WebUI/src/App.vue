d4f824096d2990005fef03a258a11e5897acd9dd
nazwa <bla@bla.pl>
2023-04-23 12:29:25 +0200

    vue-work - add axios v1

--- TakeAIMeal.WebUI/src/App.vue
+++ TakeAIMeal.WebUI/src/App.vue
@@ -3,7 +3,48 @@
 </template>
 
 <script>
+  import axios from 'axios'
   export default {
       name: 'App'
   }
+  
+  function getAxios(axios, url) {
+    axios
+      .get(url)
+      .then(function(res){
+        console.log(res.data);
+      })
+    .catch(function(err){
+      console.log(err);
+    })
+    .finally(function(){
+      // do something
+    })
+  }
+
+  function postAxios(axios, url, data) {
+    axios
+      .post(url, {
+        data: data
+      })
+  }
+
+  function deleteAxios(axios, url, data) {
+    axios
+      .delete(url, {
+        data: data
+      })
+      .then(function(response)
+      {
+        console.log(response);
+      })
+      .catch(function(err){
+        console.log(err);
+      })
+      .finally(function(){
+        // do something
+      })
+  }
+
+  getAxios(axios, 'https://api.github.com/users/section-engineering-education');
 </script>
