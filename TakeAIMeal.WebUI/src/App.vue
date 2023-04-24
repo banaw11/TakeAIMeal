ca774ba8181122e153b6c835dbfa7a13ead5e9a7
nazwa <bla@bla.pl>
2023-04-24 23:15:25 +0200

    changes

--- TakeAIMeal.WebUI/src/App.vue
+++ TakeAIMeal.WebUI/src/App.vue
@@ -5,13 +5,7 @@
 <script>
   import httpClient from './modules/http/client';
   export default {
-    name: 'App',
-    mounted(){
-      httpClient.get('https://api.github.com/users/section-engineering-education')
-        .then(function(response) {
-          console.log(response.data.login);
-        })
-    }
+    name: 'App'
   }
   
 </script>
