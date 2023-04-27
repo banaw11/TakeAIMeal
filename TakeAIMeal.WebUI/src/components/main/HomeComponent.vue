db2e51f935a99ae9f3d05bd0e3fb61fdd2c088ac
nazwa <bla@bla.pl>
2023-04-27 13:43:41 +0200

    wyswietlenie porad na stronie glownej

--- TakeAIMeal.WebUI/src/components/main/HomeComponent.vue
+++ TakeAIMeal.WebUI/src/components/main/HomeComponent.vue
@@ -5,7 +5,7 @@
     <div class="hint-container">
         <i class="icon-hint"></i>
         <div class="hint-slider">
-
+            {{ this.hintText }}
         </div>
     </div>
     <div class="action-container">
@@ -38,8 +38,17 @@
 <script>
     import { defineComponent } from 'vue'
     import { useI18n } from 'vue-i18n'
+    import httpClient from '@/modules/http/client'
     export default defineComponent({
         name: 'HomeComponent',
+        data() {
+            return {
+                hintText: '',
+                tips: [],
+                i: 0,
+                currentLanguage: ''
+            }
+        },
         setup() {
             const { t } = useI18n({
                 inheritLocale: true,
@@ -47,6 +56,43 @@
             })
 
             return { t }
+        },
+        mounted: function() {
+            let self = this;
+            self.getTips();
+            self.showTip(self.tips[0])
+            self.currentLanguage = self.$i18n.locale
+
+            // get new tips from API
+            window.setInterval(function() {
+                self.getTips();
+            }, 30000); // 5 minutes 
+
+            // reload tip
+            window.setInterval(function() {
+                if(self.currentLanguage != self.$i18n.locale) {
+                    self.currentLanguage = self.$i18n.locale;
+                    self.getTips();
+                }
+                if(self.i >= self.tips.length)
+                    self.i = 0;
+                self.showTip(self.tips[self.i]);
+            }, 5000); // 5 seconds
+            
+        },
+        methods: {
+            showTip: function(tip) {
+                this.hintText = tip;
+                this.i++;
+            },
+            getTips: function() {
+                let self = this;
+                httpClient.get('https://take-ai-meal-app.azurewebsites.net/api/Tips/' + self.$i18n.locale)
+                    .then(function(response) {
+                        self.tips = response.data;
+                        self.hintText = self.tips[0];
+                    });
+            }
         }
     })
 </script>
