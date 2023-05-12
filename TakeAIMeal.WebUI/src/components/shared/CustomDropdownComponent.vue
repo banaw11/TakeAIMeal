9ca98ad4bdd352f171ed7fd4af127c6a76f4fee0
banaw11 <banaw11@gmail.com>
2023-05-12 22:06:44 +0200

    Add custom dropdown

---
+++ TakeAIMeal.WebUI/src/components/shared/CustomDropdownComponent.vue
@@ -0,0 +1,43 @@
+<template lang="html">
+    <div>
+        <VueMultiselect class="custom-dropdown" v-model="value"
+                        :options="data"
+                        :multiple="multi"
+                        :placeholder="t('Dropdown.PleaseSelect')"
+                        :selectLabel="t('Dropdown.PressEnterForSelect')"
+                        :deselectLabel="t('Dropdown.PressEnterForRemove')"
+                        :selectedLabel="t('Dropdown.Selected')"
+                        :searchable="true"
+                        label="name"
+                        track-by="value" />
+    </div>
+</template>
+
+<script>
+    import { useI18n } from 'vue-i18n'
+    import VueMultiselect from 'vue-multiselect'
+    export default {
+        name: 'custom-multi-dropdown',
+        components: { VueMultiselect },
+        props: ['data', 'modelValue', 'multi'],
+        emits: ['update:modelValue'],
+        computed: {
+            value: {
+                get() {
+                    return this.modelValue
+                },
+                set(value) {
+                    this.$emit('update:modelValue', value)
+                }
+            }
+        },
+        setup() {
+            const { t } = useI18n({
+                inheritLocale: true,
+                useScope: 'local'
+            })
+
+            return { t }
+        }
+    };
+</script>
