d21aceb60dbcae0aca405fc743516c1ecb8a91bd
banaw11 <banaw11@gmail.com>
2023-05-14 22:15:53 +0200

    Add meal type enum and base extensions

---
+++ TakeAIMeal.Common/Extensions/EnumExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+
+namespace TakeAIMeal.Common.Extensions
+{
+    public static class EnumExtensions
+    {
+        /// <summary>
+        ///     A generic extension method that aids in reflecting 
+        ///     and retrieving any attribute that is applied to an `Enum`.
+        /// </summary>
+        public static TAttribute GetAttribute<TAttribute>(this Enum enumValue) where TAttribute : Attribute
+        {
+            return enumValue.GetType()
+                            .GetMember(enumValue.ToString())
+                            .First()
+                            .GetCustomAttribute<TAttribute>();
+        }
+    }
+}
