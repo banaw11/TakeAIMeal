433307cdf0424aea6857150677df93933abe136c
nazwa <bla@bla.pl>
2023-05-15 20:15:53 +0200

    save email validade - in progress

--- TakeAIMeal.WebUI/src/components/account/LoginComponent.vue
+++ TakeAIMeal.WebUI/src/components/account/LoginComponent.vue
@@ -1,9 +1,9 @@
 <template>
     <h1>Zaloguj się</h1>
     <form>
-        <input type="text" placeholder="E-mail" /><br/>
-        <input type="password" placeholder="Hasło" /><br/>
-        <router-link class="btn btn-primary" to="">{{ t('Header.SignIn') }}</router-link>
+        <input type="text" placeholder="E-mail" v-model.lazy="email" style="color:black" /><br/>
+        <input type="password" placeholder="Hasło" v-model.lazy="password" style="color:black" /><br/>
+        <router-link class="btn btn-primary" to="" :disabled="isInValid()">{{ t('Header.SignIn') }}</router-link>
     </form>
 </template>
 
@@ -12,6 +12,12 @@ import { defineComponent } from 'vue'
 import { useI18n } from 'vue-i18n'
 export default defineComponent({
     name: 'LoginComponent',
+    data(){
+        return {
+            email: '',
+            password: ''
+        }
+    },
     setup() {
         const { t } = useI18n({
             inheritLocale: true,
@@ -19,6 +25,21 @@ export default defineComponent({
         })
 
         return { t }
+    },
+    computed: {
+        isInValid() {
+            return () => {
+                return this.emailValidate() ? true : false;
+                // let email = this.email.length;
+                // console.log(this.emailValidate());
+                // return startValidation.value ? /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value) : null;
+            }
+        }
+    },
+    methods: {
+        emailValidate() {
+            return this.email.length > 0 ? true : false;
+        }
     }
 })
 </script>
