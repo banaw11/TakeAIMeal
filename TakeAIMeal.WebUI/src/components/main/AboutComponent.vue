dcf739bc51278d8e9cdb3305fd21faa9d6bcd5e1
nazwa <bla@bla.pl>
2023-05-28 21:05:06 +0200

    about us page

--- TakeAIMeal.WebUI/src/components/main/AboutComponent.vue
+++ TakeAIMeal.WebUI/src/components/main/AboutComponent.vue
@@ -1,12 +1,21 @@
 <template>
-  <div>
-    <img alt="Vue logo" src="@/assets/icons/logo.svg">
-    <h1>About Us</h1>
+  <div class="recipe-container">
+    <h1>{{ t('About.Header') }}</h1>
+    <p>{{ t('About.Paragraph') }}</p>
   </div>
 </template>
 
 <script>
+  import { useI18n } from 'vue-i18n'
   export default {
     name: 'AboutComponent',
+    setup() {
+      const { t } = useI18n({
+          inheritLocale: true,
+          useScope: 'local'
+      })
+
+      return { t }
+    }
   }
 </script>
